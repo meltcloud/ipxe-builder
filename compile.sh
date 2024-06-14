2a8257d2b6c8bc947fb78fca5d01a7f5120435c6
boebu <>
2024-06-14 05:41:40 +0000

    cleanup workaround

--- compile.sh
+++ compile.sh
@@ -7,23 +7,6 @@ fi
 
 . build_targets
 
-# TODO Workaround - Else the build fails because it wants a git repository
-# to determine MAJOR_VERSION based on the last tag in the Makefile but our
-# source directory is a submodule ripped from it's parent git repo.
-fix_git() {
-  cd $1
-  rm .git
-  git init
-  git config --global user.email "you@example.com"
-  git add .
-  git commit -m'Fake'
-  git tag v0.00.0-000-0000000
-  cd -
-}
-
-#fix_git ipxe_amd64
-#fix_git ipxe_aarch64
-
 cd ipxe_amd64/src
 make -j 4 ${TARGETS_AMD64}
 cd -
